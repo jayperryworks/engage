b3ae0cd3e5c4ef915d33362a83e4c0b83aa1bd75
Jay Perry <j@jayperryworks.com>
2013-10-03 12:44:17 -0400

    carousel

--- _assets/js/app.js
+++ _assets/js/app.js
@@ -1,1 +1,11 @@
+// Flexslider init
+// -------------------------------------------------------------------
 
+$(window).load(function() {
+  $('carousel.feature').flexslider({
+    animation: 'slide',
+    controlsContainer: '.carousel.nav'
+  });
+   
+  // $('#secondary-slider').flexslider();
+});
