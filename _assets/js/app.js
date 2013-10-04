345b2c1200a25cd8afd618e964bbee30d55896b0
Jay Perry <j@jayperryworks.com>
2013-10-04 01:36:16 -0400

    overall formatting, tooltips

--- _assets/js/app.js
+++ _assets/js/app.js
@@ -1,11 +1,21 @@
 // Flexslider init
 // -------------------------------------------------------------------
 
-$(window).load(function() {
-  $('carousel.feature').flexslider({
-    animation: 'slide',
-    controlsContainer: '.carousel.nav'
-  });
+// $(window).load(function() {
+//   $('carousel.feature').flexslider({
+//     animation: 'slide',
+//     controlsContainer: '.carousel.nav'
+//   });
    
-  // $('#secondary-slider').flexslider();
-});
+//   // $('#secondary-slider').flexslider();
+// });
+
+(function($) {
+	$(document).ready(function() {
+		
+	  $('.has-tooltip').tooltipsy({
+	  	offset: [0, 10]
+	  });
+
+	});
+})(jQuery);
