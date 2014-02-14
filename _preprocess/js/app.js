b8b59c341ab91e15fc91caaf854c00b5fd186df6
Jay Perry <hi@jayperryworks.com>
2014-02-13 21:38:37 -0500

    JS for tabs

--- _preprocess/js/app.js
+++ _preprocess/js/app.js
@@ -1,58 +1,129 @@
-// Flexslider init
-// -------------------------------------------------------------------
-
-// $(window).load(function() {
-//   $('carousel.feature').flexslider({
-//     animation: 'slide',
-//     controlsContainer: '.carousel.nav'
-//   });
-   
-//   // $('#secondary-slider').flexslider();
-// });
-
 (function($) {
-	$(document).ready(function() {
-		$('.has-tooltip.top').tooltipsy({
-	  	offset: [0, -10],
-	  	className: "tooltip-top"
-	  });
+  
+  // ================================================================
+  // Global vars
+  // ================================================================
 
-	  $('.has-tooltip.bottom').tooltipsy({
-	  	offset: [0, 10],
-	  	className: "tooltip-bottom"
-	  });
+  var engage = { // namespace setup
 
-	  $('.has-tooltip.left').tooltipsy({
-	  	offset: [-10, 0],
-	  	className: "tooltip-left"
-	  });
+    config : {
 
-	  // main menu show/hide
-	  // --------------------------------------------------------------
+      // settings
+      // --------------------------------------------------------------- 
+      breakpoints : 'breakpoints.json', // grab breakpoint values from a JSON file, shared by JS and SASS
+      duration : 500, // standard duration of animations
+      threshold : undefined, // threshold (vertical pixel value) where we reveal the logo icon
 
-	  var $mainMenu = $('.js #js-slide_out');
-	  var $header = $('.js #js-header-global');
-	  var $menuBtn = $('.js #js-header-menuBtn');
+      // flags
+      // ---------------------------------------------------------------
+      icon_visible : false, // is the logo icon currently visible?
 
-	  $mainMenu.hide();
-	  $menuBtn.click(menuToggle);
+    },
 
-	  $header.hover(menuHide, menuShow);
+    // Tooltips
+    // ---------------------------------------------------------------
 
-	  function menuToggle(e) {
-	  	$mainMenu.slideToggle();
-	  	return false;
-	  }
+    tooltips : function() {
+  		$('.has-tooltip.top').tooltipsy({
+		  	offset: [0, -10],
+		  	className: "tooltip-top"
+		  });
 
-	  function menuHide(e) {
-	  	$mainMenu.slideDown();
-	  	return false;
-	  }
+		  $('.has-tooltip.bottom').tooltipsy({
+		  	offset: [0, 10],
+		  	className: "tooltip-bottom"
+		  });
 
-	  function menuShow(e) {
-	  	$mainMenu.slideUp();
-	  	return false;
-	  }
+		  $('.has-tooltip.left').tooltipsy({
+		  	offset: [-10, 0],
+		  	className: "tooltip-left"
+		  });
+    },
 
-	});
+    // Carousel
+    // ---------------------------------------------------------------
+
+    carousel : function() {
+    // 	$('carousel.feature').flexslider({
+		  //   animation: 'slide',
+		  //   controlsContainer: '.carousel.nav'
+		  // });
+    },
+
+    // Tabs
+    // ---------------------------------------------------------------
+
+		tabs : {
+
+		  // settings
+		  // ----------------------------------------------------------------------------------
+		  active : "active", // the class of the active (current) tab
+		  open: "is-open", // the class of the content currently displayed
+
+		  // methods
+		  // ----------------------------------------------------------------------------------
+
+		  init : function(el) {
+		    // ----- Local nav (tabs) -------------------------------------------------------------
+		    //  -> http://www.jacklmoore.com/notes/jquery-tabs/
+
+		    $(el).each(function(){
+		      // For each set of tabs, we want to keep track of
+		      // which tab is active and it's associated content
+		      var $active, $content, $links = $(this).find('a');
+
+		      // If the location.hash matches one of the links, use that as the active tab.
+		      // If no match is found, use the first link as the initial active tab.
+		      $active = $links.filter("." + engage.tabs.active);
+		      $content = $($active.attr('href'));
+
+		      // console.log("active tab: " + $active);
+
+		      // Hide the remaining content
+		      $links.not($active).each(function () {
+		        $($(this).attr('href')).removeClass(engage.tabs.open); //hide();
+		      });
+
+		      // Bind the click event handler
+		      $(this).on('click', 'a', function(e){
+		        // Make the old tab inactive.
+		        $active.removeClass(engage.tabs.active);
+		        $content.removeClass(engage.tabs.open); //hide();
+
+		        // Update the variables with the new link and content
+		        $active = $(this);
+		        $content = $($(this).attr('href'));
+
+		        // Make the tab active.
+		        $active.addClass(engage.tabs.active);
+		        $content.addClass(engage.tabs.open); //show();
+
+		        // Prevent the anchor's default click action
+		        e.preventDefault();
+		      });
+		    });
+		  }
+		},
+
+    // Setup
+    // ---------------------------------------------------------------
+    init : function(config) {
+      $.extend(engage.config, config);
+
+
+      // fall back to .animate() frame animation is CSS transitions are not supported
+      // -> for transit.js
+      if (!$.support.transition) {
+        $.fn.transition = $.fn.animate;
+      }
+
+      engage.tabs.init('nav.local.tabs'); // set up local tabs
+      engage.tooltips();
+      engage.carousel();
+    }
+	}
+  
+	$(window).load(function() {
+    engage.init();
+  });
 })(jQuery);
